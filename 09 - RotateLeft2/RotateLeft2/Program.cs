520f1aa7ff6c42e1586947d889e94770b5e9d644
Jim Shaw <jimxshaw@gmail.com>
2015-09-15 19:32:49 -0400

    warmup 09

---
+++ 09 - RotateLeft2/RotateLeft2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateLeft2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Input a word: ");
+            string input = Console.ReadLine();
+            Console.WriteLine(RotateLeft2(input));
+            Console.ReadLine();
+        }
+
+        public static string RotateLeft2(string str)
+        {
+            string left2 = str.Remove(2);
+            string remainingLetters = str.Remove(0, 2);
+
+            return remainingLetters + left2;
+        }
+    }
+}
