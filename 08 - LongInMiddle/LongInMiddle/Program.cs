8cea4014d77d2ce38a5c3ba795f64bec5217a621
Jim Shaw <jimxshaw@gmail.com>
2015-09-15 16:29:19 -0400

    warmup 8

---
+++ 08 - LongInMiddle/LongInMiddle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongInMiddle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input string A: ");
+            string inputA = Console.ReadLine();
+            Console.Write("Input string B: ");
+            string inputB = Console.ReadLine();
+            Console.WriteLine(LongInMiddle(inputA, inputB));
+
+            Console.ReadLine();
+        }
+
+        public static string LongInMiddle(string a, string b)
+        {
+            if (a.Length > b.Length)
+            {
+                return b + a + b;
+            }
+            else
+            {
+                return a + b + a;
+            }
+        }
+    }
+}
