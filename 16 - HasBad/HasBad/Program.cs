9833ad586ada07aea31839e37a202889bfab1bc5
Jim Shaw <jimxshaw@gmail.com>
2015-09-16 15:23:33 -0400

    warmup 16

---
+++ 16 - HasBad/HasBad/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HasBad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a string containing the word 'bad': ");
+            string word = Console.ReadLine();
+            Console.WriteLine(HasBad(word));
+
+            Console.ReadLine();
+        }
+
+        public static bool HasBad(string str)
+        {
+            if (!str.Contains("bad"))
+            {
+                return false;
+            }
+
+            if (str.IndexOf("b") == 0 || str.IndexOf("b") == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
