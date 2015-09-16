e68616366dff88ead4e24d5e0e980bf5cd1e0198
Jim Shaw <jimxshaw@gmail.com>
2015-09-16 15:37:56 -0400

    warmup 17

---
+++ 17 - AtFirst/AtFirst/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a word: ");
+            string word = Console.ReadLine();
+            Console.WriteLine(AtFirst(word));
+
+            Console.ReadLine();
+        }
+
+        public static string AtFirst(string str)
+        {
+            if (str.Length == 2)
+            {
+                return str;
+            }
+
+            if (str.Length < 2)
+            {
+                return str + "@";
+            }
+
+            return str.Remove(2);
+        }
+    }
+}
