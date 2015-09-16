d9760801af7f9de159dc02cbcf1a58b840b2a0ae
Jim Shaw <jimxshaw@gmail.com>
2015-09-15 22:27:14 -0400

    warmup 13 & 14

---
+++ 13 - EndsWithLy/EndsWithLy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndsWithLy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a word: ");
+            string word = Console.ReadLine();
+            Console.WriteLine(EndsWithLy(word));
+
+            Console.ReadLine();
+        }
+
+        public static bool EndsWithLy(string str)
+        {
+            if (str.EndsWith("ly"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 14 - FrontAndBack/FrontAndBack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontAndBack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a word: ");
+            string word = Console.ReadLine();
+            Console.Write("Input an integer that's fewer than the length of your word: ");
+            string numString = Console.ReadLine();
+            int number = Int32.Parse(numString);
+
+            Console.WriteLine(FrontAndBack(word, number));
+            Console.ReadLine();
+        }
+
+        public static string FrontAndBack(string str, int n)
+        {
+            string front = str.Remove(n);
+            string back = str.Remove(0, str.Length - n);
+
+            return front + back;
+        }
+    }
+}
