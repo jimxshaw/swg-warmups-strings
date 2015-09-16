b73676fa38e9cce1e5392f51222d8cb23c08cf59
Jim Shaw <jimxshaw@gmail.com>
2015-09-15 20:33:16 -0400

    warmup 11

---
+++ 11 - TakeOne/TakeOne/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a word: ");
+            string word = Console.ReadLine();
+            Console.Write("Take first letter? (y or n): ");
+            string fromFront = Console.ReadLine();
+
+    
+            if (fromFront == "y")
+            {
+                Console.WriteLine(TakeOne(word, true));
+            }
+            else if (fromFront == "n")
+            {
+                Console.WriteLine(TakeOne(word, false));
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static string TakeOne(string str, bool fromFront)
+        {
+            if (fromFront)
+            {
+                return str.Remove(1);
+            }
+            else
+            {
+                return str.Remove(0, str.Length - 1);
+            }
+        }
+    }
+}
