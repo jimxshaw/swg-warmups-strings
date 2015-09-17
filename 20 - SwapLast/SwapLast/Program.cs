070855b7c09de17fae4654a6f4808926d9c998b3
Jim Shaw <jimxshaw@gmail.com>
2015-09-16 21:11:32 -0400

    warmup 20

---
+++ 20 - SwapLast/SwapLast/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapLast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a word: ");
+            string word = Console.ReadLine();
+
+            Console.WriteLine(SwapLast(word));
+            Console.ReadLine();
+        }
+
+        public static string SwapLast(string str)
+        {
+            if (str.Length < 2)
+            {
+                return "Insufficient word length.";
+            }
+            if (str.Length == 2)
+            {
+                return str.Substring(1) + str.Substring(0, 1);
+            }
+            string sub = str.Remove(str.Length - 2);
+            string subA = str.Substring(str.Length - 2, 1);
+            string subB = str.Substring(str.Length - 1);
+
+            return sub + subB + subA;
+        }
+    }
+}
