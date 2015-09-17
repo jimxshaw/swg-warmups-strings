bf2593b0d6bfe512d3da68e52ad41e8ca3ac82e5
Jim Shaw <jimxshaw@gmail.com>
2015-09-16 22:53:00 -0400

    warmup 24

---
+++ 24 - StripX/StripX/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StripX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a word: ");
+            string word = Console.ReadLine();
+
+            Console.WriteLine(StripX(word));
+            Console.ReadLine();
+        }
+
+        public static string StripX(string str)
+        {
+            if (str.StartsWith("x") && str.EndsWith("x"))
+            {
+                return str.Substring(1, str.Length - 2);
+            }
+            if (str.StartsWith("x"))
+            {
+                str = str.Remove(0, 1);
+                return str;
+            }
+            if (str.EndsWith("x"))
+            {
+                str = str.Remove(str.Length - 1);
+                return str;
+            }
+
+            return str;
+        }
+    }
+}
