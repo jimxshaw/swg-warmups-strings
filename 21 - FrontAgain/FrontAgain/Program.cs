d823d6af721a88747a9e23ea7af50368a94dbab9
Jim Shaw <jimxshaw@gmail.com>
2015-09-16 22:02:30 -0400

    warmup 21 & 22

---
+++ 21 - FrontAgain/FrontAgain/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a word: ");
+            string word = Console.ReadLine();
+
+            Console.WriteLine(FrontAgain(word));
+            Console.ReadLine();
+        }
+
+        public static bool FrontAgain(string str)
+        {
+            if (str.Length < 2)
+            {
+                return false;
+            }
+            if (str.Length == 2)
+            {
+                return true;
+            }
+            if (str.Substring(0, 2) == str.Substring(str.Length - 2))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 22 - MinCat/MinCat/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinCat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input first word: ");
+            string wordA = Console.ReadLine();
+            Console.Write("Input second word: ");
+            string wordB = Console.ReadLine();
+
+            Console.WriteLine(MinCat(wordA, wordB));
+
+            Console.ReadLine();
+        }
+
+        public static string MinCat(string a, string b)
+        {
+            if (a.Length == b.Length)
+            {
+                return a + b;
+            }
+            if (a.Length > b.Length)
+            {
+                int differenceA = a.Length - b.Length;
+
+                a = a.Remove(0, differenceA);
+
+                return a + b;
+            }
+
+            int differenceB = b.Length - a.Length;
+
+            b = b.Remove(0, differenceB);
+
+            return a + b;
+        }
+    }
+}
